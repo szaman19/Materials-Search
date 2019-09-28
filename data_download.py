b34ec14cd413910b72ead204b3aebb882e88c8fa
Shehtab Zaman <shehtabzaman@gmail.com>
2019-09-28 19:55:11 -0400

    Updated downloader with csv file for properties

--- data_download.py
+++ data_download.py
@@ -1,15 +1,28 @@
 import requests 
 
-def main():
-	print('Beginning file download with requests')
 
-	url = 'https://zenodo.org/record/3370144/files/2019-07-01-ASR-public_12020.tar?download=1'
+def downloader(link, file_name):
+	url = link
 	r = requests.get(url)
 
-	with open('data/2019-07-01-ASR-public_12020.tar', 'wb') as f:
+	with open(file_name, 'wb') as f:
 		f.write(r.content)
-	# Retrieve HTTP meta-data
-	print(r.status_code)
-	print(r.headers['content-type'])
-	print(r.encoding)
+		f.close()
+	if (r.status_code == "200"):
+		print("Completed download")
+
+
+
+def main():
+	print('Beginning cif download')
+	url_cif = 'https://zenodo.org/record/3370144/files/2019-07-01-ASR-public_12020.tar?download=1'
+	cif_tar_name = '2019-07-01-ASR-public_12020.tar'
+	downloader(url_cif, cif_tar_name)
+	
+	print('Beginning csv download')
+	url_csv = "https://zenodo.org/record/3370144/files/2019-07-01-ASR-public_12020.csv?download=1"
+	csv_name = "2019-07-01-ASR-public_12020.csv"
+	downloader(url_csv, csv_name)
+	
+        
 main()
