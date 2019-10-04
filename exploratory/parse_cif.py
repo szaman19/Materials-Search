b0a381af61bfccfff1bd049a790426509dd0e825
Shehtab Zaman <shehtabzaman@gmail.com>
2019-10-03 22:47:04 -0400

    updated to run parser

--- exploratory/parse_cif.py
+++ exploratory/parse_cif.py
@@ -3,6 +3,9 @@ import sys
 from pymatgen.io.cif import CifParser
 import os
 import glob
+import numpy as np 
+import networkx as nx
+import matplotlib.pyplot as plt
 '''
 Given a structure, find the number of unique elements present
 and return a list of elements
@@ -17,6 +20,9 @@ def num_species(structure):
 			species.add(each['element'])
 	return species
 
+def num_elements(structure):
+	sites = structure.as_dict()['sites']
+	return len(sites)
 '''
 Given a valid cif filename, returns a pymatgen structure object
 
@@ -31,23 +37,31 @@ def main():
 	files = glob.glob("*.cif")
 	print(len(files))
 	
-	total_unique_species = set()
+	# structure = cif_structure(files[10])
+	# distance_matrix = structure.distance_matrix
+	# print(files[0])
+	# print(distance_matrix.shape)
+	# graph = nx.from_numpy_matrix(distance_matrix)
+	
+	# nx.draw(graph)
+	# plt.show()
+	# total_unique_species = set()
 
-	file_write = open("species.dat","w")
+	file_write = open("sizes.dat","w")
 	for file in files:
 		structure = cif_structure(file)
-		u_elements = num_species(structure)
-		total_unique_species.union(u_elements)
+		u_elements = num_elements(structure)
+		# total_unique_species.union(u_elements)
 		file_write.write(file)
 		file_write.write(" ")
 		file_write.write(str(u_elements))
 		file_write.write("\n")
 		print(u_elements)
-	file_write.close()
+	# file_write.close()
 	print("*"*80)
 
-	print("Total number of unique elements in the dataset: ", len(total_unique_species))
-	print(total_unique_species)
+	# print("Total number of unique elements in the dataset: ", len(total_unique_species))
+	# print(total_unique_species)
 
 
 main()
