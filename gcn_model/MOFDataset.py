90641aadd5e020c7f538ca7b3376e63fd21132dd
Shehtab Zaman <shehtabzaman@gmail.com>
2019-10-24 23:38:26 -0400

    Added dataloader for gcn model

--- gcn_model/MOFDataset.py
+++ gcn_model/MOFDataset.py
@@ -1,7 +1,11 @@
 import torch 
 import os
 from torch_geometric.data import Data
+from pymatgen.io.cif import CifParser
 import pandas as pd
+import numpy as np
+import torch_geometric.utils
+import networkx as nx
 
 
 class MOFDataset():
@@ -11,15 +15,71 @@ class MOFDataset():
 		super(MOFDataset, self).__init__()
 
 		if train:
-			self.data_dir = "/data/train/"
+			self.data_dir = "/data/training/"
 		else:
 			self.data_dir = "/data/test/"
 
 	def get_data(self):
 		""" returns a list of Data objects """
-		print(os.getcwd() + self.data_dir)
-		files = glob.glob("*.cif")
+		directory = os.getcwd() + self.data_dir
+		# print(type(directory)) 
+		# files = glob.glob(self.data_dir+"*.cif")
+		labels = pd.read_csv(directory+"properties.csv")
+		# counter = 1
 
-		prop = "property.csv"
-		
+
+		dataset = []
+
+		counter = 0
+
+		# print(feature_matrix)
+
+		for file in labels['filename']:
+			if(os.path.exists(directory+file+".cif")):
+				file  = labels['filename'][counter]
+				structure = self.cif_structure(directory+file+".cif")
+				distance_matrix = structure.distance_matrix
+				graph = nx.from_numpy_matrix(distance_matrix)
+				num_nodes = distance_matrix.shape[0]
+				# print(num_nodes)
+				feature_matrix = self.get_feature_matrix(structure)
+				
+				data = torch_geometric.utils.from_networkx(graph)
+				data.x = torch.from_numpy(feature_matrix)
+				data.y = labels['LCD'][counter]	
+
+				dataset.append(data)
+				counter +=1
+
+			else:
+				print("Not ok skipping: ", file)
+
+	def cif_structure(self,file_name):
+		parser = CifParser(file_name)
+		structure = parser.get_structures()[0]
+		return structure	
+
+	def one_hot_encode(self, element):
+		elements = ["H","N","C","O","Co","P","Zn","Ag","Cd","Cu","Fe"]
+
+		one_hot_vector = np.zeros(len(elements))
+		one_hot_vector[elements.index(element)] = 1
+		return one_hot_vector
+
+	def one_hot_test(self, val = 0, element="H"):
+		one_hot = self.one_hot_encode(element)
+		print(one_hot)
+
+		true_val = np.zeros(11)
+		true_val[val] = 1
+		print(true_val)
+		print(true_val == self.one_hot_encode(element))
+	def get_feature_matrix(self, structure):
+		arr = []
+		for each in structure.sites:
+			vec = self.one_hot_encode(str(each.specie))
+			arr.append(vec)
+		return np.array(arr)
+
+
 
--- gcn_model/model.py
+++ gcn_model/model.py
@@ -1,17 +1,19 @@
 import torch 
+import torch.nn.functional as F
 from torch_geometric.data import DataLoader
 from torch_geometric.nn import GraphConv, TopKPooling
 from torch_geometric.nn import global_mean_pool as gap
 from torch_geometric.nn import global_max_pool as gmp
+
 import MOFDataset
 
 class Net(torch.nn.Module):
 	"""docstring for Net"""
-	def __init__(self):
+	def __init__(self, num_features):
 		super(Net, self).__init__()
 		
 		#channel in is  size of input features, channel out is 128
-		self.conv1 = GraphConv(dataset.num_features, 128) 
+		self.conv1 = GraphConv(num_features, 128) 
 
 		#channel in is 128, channel out is 128. Ratio is 0.8
 		self.pool1 = TopKPooling(128, ratio=0.8)
@@ -30,10 +32,39 @@ class Net(torch.nn.Module):
 		self.lin2 = torch.nn.Linear(128,64)
 		self.lin3 = torch.nn.Linear(64, 1) #Continuous output
 
+	def forward(self, forward):
+		x, edge_index, batch = data.x, data.edge_index, data.batch
+
+	
+		x = F.relu(self.conv1(x, edge_index))
+		x, edge_index, _, batch, _, _ = self.pool1(x,edge_index,None,batch)
+		x1 = torch.cat([gmp(x,batch), gap(x,batch)],dim =1)
+
+		x = F.relu(self.conv2(x, edge_index))
+		x, edge_index, _, batch, _, _ = self.pool2(x, edge_index, None, batch)
+		x2 = torch.cat([gmp(x,batch), gap(x,batch)], dim=1)
+
+		x = F.relu(self.conv3(x, edge_index))
+		x, edge_index, _, batch, _, _ = self.pool3(x, edge_index, None, batch)
+		x3 = torch.cat([gmp(x,batch), gap(x,batch)], dim=1)
+
+		x = x1 + x2 + x3 
+
+		
+		x = F.relu(self.lin1(x))
+		x = F.dropout(x, p=0.5, training = self.training)
+		x = F.relu(self.lin2(x))
+		x = self.lin3(x)
+
 def main():
-	training_data_obj = MOFDataset.MOFDataset(train=True).get_data()
-	# model = Net()
-	# criterion = torch.nn.MSEloss()
+	training_data_list = MOFDataset.MOFDataset(train=True).get_data()
+
+	loader = DataLoader(training_data_list, batch_size = 16, shuffle = True)
+
+	print(len(loader))
+	# training_data_obj.one_hot_test(4,"Co")
+	# model = Net(11)
+	# criterion = torch.nn.MSELoss()
 	# optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
 	
 if __name__ == '__main__':
