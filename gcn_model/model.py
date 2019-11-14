2ecbd782d65ac138cf9e982805d01d85f5e399fb
Shehtab Zaman <szaman5@titan.cs.binghamton.edu>
2019-11-13 22:34:25 -0500

    Adding sparse results

--- gcn_model/model.py
+++ gcn_model/model.py
@@ -82,7 +82,7 @@ def main():
 	model = Net(11).to(device)
 	criterion = torch.nn.MSELoss()
 	optimizer = torch.optim.Adam(model.parameters(), lr=3E-4)
-	epoch = 100
+	epoch = 20
 	print("Starting Training:")
 	print("*"*40)
 	for i in range(epoch):
