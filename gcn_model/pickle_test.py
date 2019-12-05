aed319d706f2aecff06d8b73e188435f459f6f33
Shehtab Zaman <shehtabzaman@gmail.com>
2019-12-05 00:36:50 -0500

    Updated dense data

--- gcn_model/pickle_test.py
+++ gcn_model/pickle_test.py
@@ -12,11 +12,11 @@ import MOFDataset
 import pickle
 
 def main():
-	file = open("inverse_sparse_test_data_PLD.p", 'wb')
+	file = open("inverse_dense_test_data_PLD.p", 'wb')
 	training_data_list = MOFDataset.MOFDataset(train=False).get_data()
 	pickle.dump(training_data_list, file)
 
-	train_file = open("inverse_sparse_train_data_PLD.p", 'wb')
+	train_file = open("inverse_dense_train_data_PLD.p", 'wb')
 	dl = MOFDataset.MOFDataset(train=True).get_data()
 	pickle.dump(dl, train_file)
 
