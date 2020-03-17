0c3924540c5c87ac50471d9f48aaf961ddac0935
Shehtab Zaman <shehtabzaman@gmail.com>
2020-03-17 13:31:05 -0400

    Added energy grid calculation file generators for Musen

---
+++ 3DGen_Model/VAE.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn 
+from torch.autograd import Variable 
+
+
+class Encoder(nn.Module):
+
+	def __init__(self):
+		super(Encoder, self).__init__()
+
+class Decoder(nn.Module):
+	"""docstring for Decoder"""
+	def __init__(self, arg):
+		super(Decoder, self).__init__()
+class VAE(nn.Module):
+	
+		
---
+++ Energy_Grid_Gen/inp_file_generator.py
@@ -0,0 +1,18 @@
+import glob 
+import subprocess
+
+def main():
+	training_files = glob.glob("data/training/*.cif")
+	test_files = glob.glob("data/test/*.cif")
+	
+	for file in training_files:
+		file_name = file[14:]
+		save_file_name = "input_data/training/"+file_name[:-4]+".inp"
+		subprocess.run(["./ciftoinput",file_name,"data_ff_UFF",save_file_name ])
+
+	for file in test_files:
+		file_name = file[10:]
+		save_file_name = "input_data/test/"file_name[:-4]+".inp"
+		subprocess.run(["./ciftoinput",file_name,"data_ff_UFF",save_file_name ])
+
+main()
