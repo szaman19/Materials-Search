b1e10b4ba6ccf68644397b7b3bf792e105b668f7
Shehtab Zaman <shehtabzaman@gmail.com>
2020-03-26 19:12:34 -0400

    Multiprocessing version of inp_file_generator cause why not

--- 3D_Grid_Data/inp_file_generator.py
+++ 3D_Grid_Data/inp_file_generator.py
@@ -1,18 +1,28 @@
 import glob 
 import subprocess
 
+
+def train_files_gen(file):
+	file_name = file[14:]
+	save_file_name = "input_data/training/"+file_name[:-4]+".inp"
+	ret = subprocess.run(["./cif2input",file_name,"data_ff_UFF",save_file_name ])
+	return ret.returncode
+
+def test_files_gen(file):
+	file_name = file[10:]
+	save_file_name = "input_data/test/"+file_name[:-4]+".inp"
+	ret = subprocess.run(["./cif2input",file_name,"data_ff_UFF",save_file_name ])
+	return ret.returncode 
+
 def main():
 	training_files = glob.glob("data/training/*.cif")
 	test_files = glob.glob("data/test/*.cif")
 	
-	for file in training_files:
-		file_name = file[14:]
-		save_file_name = "input_data/training/"+file_name[:-4]+".inp"
-		subprocess.run(["./cif2input",file_name,"data_ff_UFF",save_file_name ])
+	pool = mp.Pool(processes=40)
+	results = pool.map(train_files_gen, training_files)
+	print(results)
 
-	for file in test_files:
-		file_name = file[10:]
-		save_file_name = "input_data/test/"+file_name[:-4]+".inp"
-		subprocess.run(["./cif2input",file_name,"data_ff_UFF",save_file_name ])
-
+	pool = mp.Pool(processes=40)
+	results = pool.map(test_files_gen, test_files)
+	print(results)
 main()
