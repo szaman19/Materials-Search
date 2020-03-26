d9c213562e799785d28d2d2d58ad985e092b5a02
Shehtab Zaman <szaman5@titan.cs.binghamton.edu>
2020-03-26 19:06:38 -0400

    Update inp_generated_file to fix typo

--- 3D_Grid_Data/inp_file_generator.py
+++ 3D_Grid_Data/inp_file_generator.py
@@ -8,11 +8,11 @@ def main():
 	for file in training_files:
 		file_name = file[14:]
 		save_file_name = "input_data/training/"+file_name[:-4]+".inp"
-		subprocess.run(["./ciftoinput",file_name,"data_ff_UFF",save_file_name ])
+		subprocess.run(["./cif2input",file_name,"data_ff_UFF",save_file_name ])
 
 	for file in test_files:
 		file_name = file[10:]
-		save_file_name = "input_data/test/"file_name[:-4]+".inp"
-		subprocess.run(["./ciftoinput",file_name,"data_ff_UFF",save_file_name ])
+		save_file_name = "input_data/test/"+file_name[:-4]+".inp"
+		subprocess.run(["./cif2input",file_name,"data_ff_UFF",save_file_name ])
 
 main()
