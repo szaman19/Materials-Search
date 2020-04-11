d9ca0288755d33085e598babff0b410058a7be40
Shehtab Zaman <shehtabzaman@gmail.com>
2020-04-11 16:15:39 -0400

    Adding 3d-cnn forward pass data

---
+++ cnn_model/infer_lattice_params.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn 
+from torch.autograd import Variable 
+
+from mof_dataset import MOFDataset 
+
+
+class Conv_Model(nn.Module):
+ 	"""docstring for Conv_Model"""
+ 	def __init__(self, num_features, z_dim):
+ 		super(Conv_Model, self).__init__()
+ 		
+ 		self.Num_features = num_features
+		self.Z_dim = z_dim
+
+		self.main = nn.Sequential(
+			nn.Conv3d(self.Num_features, self.Z_dim // 8, 4,2,1), #32
+			nn.BatchNorm3d(self.Z_dim // 8),
+			nn.LeakyReLU(0.2), 
+
+			nn.Conv3d(self.Z_dim // 8, self.Z_dim // 4, 4,2,1), #16
+			nn.BatchNorm3d(self.Z_dim // 4),
+			nn.LeakyReLU(0.2), 
+
+			nn.Conv3d(self.Z_dim // 4, self.Z_dim // 2, 4,2,1), #4
+			nn.BatchNorm3d(self.Z_dim // 2),
+			nn.LeakyReLU(0.2), 
+
+			nn.Conv3d(self.Z_dim // 2, self.Z_dim, 4,2,1), #2
+			nn.BatchNorm3d(self.Z_dim),
+			nn.LeakyReLU(0.2),
+
+			nn.Conv3d(self.Z_dim, self.Z_dim, 2,2,0), #1
+			)
+		def forward(self, x):
+			x = self.main(x)
+			x = x.view(-1, x.size(0))
+
+			return x 
+
+
+def main():
+	epoch = 100
+	batch_size = 32 
+	learning_rate = 1e-4
+
+	num_features = 12 
+	grid_size = 32 
+
+	train_loader = MOFDataset.get_data_loader("../3D_Grid_data/Test_MOFS.p")
+
+if __name__ == '__main__':
+
+	main()
+ 		 
---
+++ cnn_model/mof_dataset.py
@@ -0,0 +1,44 @@
+import pickle
+from pathlib import Path
+from typing import List
+
+import torch
+from torch.utils.data import Dataset
+
+import Voxel_MOF
+
+
+class MOFDataset(Dataset):
+    def __init__(self, path, transform=None):
+        self.path = path
+        path = Path(path)
+        with path.open("rb") as f:
+            self.data: List[Voxel_MOF] = pickle.load(f)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx].data
+
+    @staticmethod
+    def get_data_loader(path: str, batch_size: int):
+        return torch.utils.data.DataLoader(
+            MOFDataset(path),
+            batch_size=batch_size,
+            shuffle=True,
+        )
+
+
+def main():
+    data_loader = MOFDataset.get_data_loader("../3D_Grid_Data/Test_MOFS.p", 25)
+
+    batch: int
+    mofs: torch.Tensor
+    for batch, mofs in enumerate(data_loader):
+        print(batch, mofs.shape)
+
+
+if __name__ == '__main__':
+    main()
