2fec9e5dd383f71b578252f7da33e40093867089
Shehtab Zaman <szaman5@binghamton.edu>
2020-09-04 09:34:23 -0400

    Updated MOLGCN.py

--- MOF_Force_Field/MOLGCN.py
+++ MOF_Force_Field/MOLGCN.py
@@ -6,9 +6,13 @@ from torch_geometric.nn.conv import MessagePassing
 
 class MOLGCN(MessagePassing):
 	"""docstring for MOLGCN"""
-	def __init__(self, nn):
+	def __init__(self,
+				 nn, 
+				 aggr = 'add',
+				 **kwargs):
 		super(MOLGCN, self).__init__()
 		self.nn = nn
+		self.aggr = aggr
 
 	def reset_parameters(self):
 		self.nn.reset_parameters()
@@ -18,10 +22,14 @@ class MOLGCN(MessagePassing):
 		if isinstance(x, Tensor):
 			x: PairTensor = (x, x)
 
-		
+		out = self.propagate(edge_index, x = x, edge_attr = edge_attr)
+
+		return out 
 
 
+	def message(self, x_i, x_j, edge_attr):
+		z = torch.cat([x_i, x_j, edge_attr], dim = -1)
+		return self.nn(z)
 
-	def message(self):
 
 		
